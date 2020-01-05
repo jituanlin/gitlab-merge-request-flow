757c3f747c2273d1ecb70083a5b12bde481a4863
jituanlin <linjituan@guorou.tv>
2020-01-05 17:38:17 +0800

    refactor file structure

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,5 @@
 import * as simplegit from 'simple-git/promise';
-import {GITLAB_HOST, GITLAB_TOKEN, PROJECT_ID, TARGET_BRANCH, WORKING_BRANCH} from "./secret";
+import {GITLAB_HOST, GITLAB_TOKEN, PROJECT_ID, TARGET_BRANCH, WORKING_BRANCH} from "../secret";
 import {Gitlab as GitlabClient} from "gitlab";
 
 export const gitClient = simplegit();
